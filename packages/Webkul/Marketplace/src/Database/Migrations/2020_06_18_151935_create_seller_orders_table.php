ef2851359bc6b57cdba9cf4b83e2a56c0830a661
unknown <abdullah@wewebit.com>
2020-12-05 21:57:56 +0200

    fix migration

--- packages/Webkul/Marketplace/src/Database/Migrations/2020_06_18_151935_create_seller_orders_table.php
+++ packages/Webkul/Marketplace/src/Database/Migrations/2020_06_18_151935_create_seller_orders_table.php
@@ -19,7 +19,7 @@ class CreateSellerOrdersTable extends Migration
             $table->integer('order_id')->unsigned()->nullable();
             $table->foreign('seller_id')->references('id')->on('sellers')->onDelete('cascade');
             $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
-            
+
             $table->string('channel_name')->nullable();
             $table->boolean('is_guest')->nullable();
             $table->string('customer_email')->nullable();
@@ -31,10 +31,10 @@ class CreateSellerOrdersTable extends Migration
             $table->string('shipping_description')->nullable();
             $table->string('coupon_code')->nullable();
             $table->boolean('is_gift')->default(0);
-            
+
             $table->string('status')->nullable();
             $table->string('state')->nullable();
-            
+
             $table->decimal('grand_total', 12, 4)->default(0)->nullable();
             $table->decimal('base_grand_total', 12, 4)->default(0)->nullable();
             $table->decimal('sub_total', 12, 4)->default(0)->nullable();
@@ -63,13 +63,13 @@ class CreateSellerOrdersTable extends Migration
             $table->decimal('base_discount_invoiced', 12, 4)->default(0)->nullable();
             $table->decimal('tax_amount_invoiced', 12, 4)->default(0)->nullable();
             $table->decimal('base_tax_amount_invoiced', 12, 4)->default(0)->nullable();
-            
+
             $table->decimal('seller_total_invoiced', 12, 4)->default(0)->nullable();
             $table->decimal('base_seller_total_invoiced', 12, 4)->default(0)->nullable();
             $table->decimal('shipping_invoiced', 12, 4)->default(0)->nullable();
             $table->decimal('base_shipping_invoiced', 12, 4)->default(0)->nullable();
             $table->decimal('total_paid',12,4)->default(0)->nullable();
-            
+
             $table->decimal('grand_total_refunded', 12, 4)->default(0)->nullable();
             $table->decimal('base_grand_total_refunded', 12, 4)->default(0)->nullable();
             $table->decimal('sub_total_refunded', 12, 4)->default(0)->nullable();
@@ -80,19 +80,19 @@ class CreateSellerOrdersTable extends Migration
             $table->decimal('base_tax_amount_refunded', 12, 4)->default(0)->nullable();
             $table->decimal('shipping_refunded', 12, 4)->default(0)->nullable();
             $table->decimal('base_shipping_refunded', 12, 4)->default(0)->nullable();
-            
+
             $table->integer('customer_id')->unsigned()->nullable();
             $table->foreign('customer_id')->references('id')->on('customers')->onDelete('set null');
-            
+
             $table->timestamp('created_at')->nullable();
             //
         });
-        
+
         Schema::table('order_items', function($table) {
-            $table->bigIncrements('seller_order_id')->nullable();
+            $table->unsignedBigInteger('seller_order_id')->nullable();
             $table->foreign('seller_order_id')->references('id')->on('seller_orders')->onDelete('set null');
         });
-        
+
     }
 
     /**
